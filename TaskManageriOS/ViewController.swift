cd5a8f4237234e4482c7461b6b8bbfe3dede19b0
Matthew Riley <matthewriley@Interapts-MacBook-Pro-10.local>
2018-11-01 08:52:55 -0500

    Initial Commit

---
+++ TaskManageriOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaskManageriOS
+//
+//  Created by Matthew Riley on 11/1/18.
+//  Copyright © 2018 Matthew Riley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
