d67f885c73247ac6cc8fccf243856dd8afb615b5
Matthew Riley <matthewriley@Interapts-MacBook-Pro-10.local>
2018-11-01 14:05:57 -0500

    1. Created AddNewTaskViewController.swift file and linked it to the AddNewTask view controller

---
+++ TaskManageriOS/ViewControllers/AddNewTaskViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddNewTaskViewController.swift
+//  TaskManageriOS
+//
+//  Created by Matthew Riley on 11/1/18.
+//  Copyright © 2018 Matthew Riley. All rights reserved.
+//
+
+import UIKit
+
+class AddNewTaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
