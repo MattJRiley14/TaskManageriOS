b1d61808e0bd38f0d6a2afd22bf8ea225cc74b29
Matthew Riley <matthewriley@Interapts-MacBook-Pro-10.local>
2018-11-02 10:22:23 -0500

    1. Created TaskManager class 2. Created Utilities file

--- TaskManageriOS/Utilities/TaskManager.swift
+++ TaskManageriOS/Utilities/TaskManager.swift
@@ -8,6 +8,41 @@
 
 import Foundation
 
+class TaskManager {
+    var taskArray: [Task] = [
+        Task(title: "Go to Wal-Mart", taskDetails: "Buy everything on my list and Lauren's list"),
+        Task(title: "Call Papi", taskDetails: "Ask him if he needs anything from the grocery store"),
+        Task(title: "Shave", taskDetails: "Find charger for beard trimmer")
+    ]
+
+    
+    func getTaskCount() -> Int {
+        return taskArray.count
+    }
+    
+    func getTask(at index: Int) -> Task {
+        return taskArray[index]
+    }
+    
+    func addTask(task: Task) {
+        taskArray.append(task)
+    }
+    
+    func removeTask(at index: Int) {
+        taskArray.remove(at: index)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ TaskManageriOS/Utilities/Utilities.swift
@@ -0,0 +1,16 @@
+//
+//  Utilities.swift
+//  TaskManageriOS
+//
+//  Created by Matthew Riley on 11/2/18.
+//  Copyright © 2018 Matthew Riley. All rights reserved.
+//
+
+import Foundation
+
+
+
+
+
+
+
