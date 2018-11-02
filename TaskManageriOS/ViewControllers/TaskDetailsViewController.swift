f6fabecc3245c6fafc27ad8107e297eec83e03b2
Matthew Riley <matthewriley@Interapts-MacBook-Pro-10.local>
2018-11-02 10:48:58 -0500

    Created TaskDetailsViewController file

---
+++ TaskManageriOS/ViewControllers/TaskDetailsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TaskDetailsViewController.swift
+//  TaskManageriOS
+//
+//  Created by Matthew Riley on 11/2/18.
+//  Copyright © 2018 Matthew Riley. All rights reserved.
+//
+
+import UIKit
+
+class TaskDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
